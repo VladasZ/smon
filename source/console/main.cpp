df946771d79ce694ceb73c83e0750517393f2453
Vladas Zakrevskis <146100@gmail.com>
2018-09-24 23:32:58 +0300

    build fix

--- source/console/main.cpp
+++ source/console/main.cpp
@@ -1,6 +1,30 @@
 
-int main()
-{
+#include <iostream>
 
+#include "SerialMonitor.hpp"
+
+using namespace std;
+
+SerialMonitor serial("/dev/cu.usbmodemFA131", 9600);
+
+
+int main() {
+    
+    
+    while (1)
+    {
+        string message;
+        
+        cin >> message;
+        
+        serial.writeString(message);
+        
+        cout << "Cout " << message << endl;
+
+        cout << serial.readLine() << endl;
+    }
+    
+    //cout << serial.readLine() << endl;
+    
 	return 0;
 }
