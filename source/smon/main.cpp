9c8ecaaefea4483067214311f5aa1d9740f28544
Vladas Zakrevskis <146100@gmail.com>
2018-11-14 14:04:19 +0300

    updated build system

--- source/smon/main.cpp
+++ source/smon/main.cpp
@@ -2,9 +2,14 @@
 #include <iostream>
 #include <thread>
 
+#include <boost/filesystem.hpp>
+
 using namespace std;
 
 int main() {
   cout << "Hello spes" << endl;
+
+  cout << boost::filesystem::exists("spes") << endl;
+  
   return 0;
 }
