77fd38fbd9e35dc6b51d1bfc5e8c0e5bd9eae776
Vladas <146100@gmail.com>
2020-04-23 21:54:53 +0300

    simplified base serial monitor

--- source/smon/SerialMonitor.hpp
+++ source/smon/SerialMonitor.hpp
@@ -1,46 +1,26 @@
 
 #pragma once
 
-#include <list>
-#include <array>
-#include <mutex>
-#include <thread>
-
-#include "Log.hpp"
-#include "DataBuffer.hpp"
-#include "DataPacket.hpp"
-
 namespace smon {
 
     class SerialMonitor {
 
     public:
 
-        static inline unsigned bytes_received = 0;
-        static inline unsigned bytes_sent = 0;
-
-        bool failed_init = false;
-
         explicit SerialMonitor(const std::string& port, unsigned baud_rate);
 
         ~SerialMonitor();
 
         template<class T>
-        T& read() {
-            static T value;
-            value = T{};
+        T read() {
+            T value;
             read(value);
             return value;
         }
 
         template<class T>
         void read(T& value) {
-            if constexpr (cu::is_data_v<T>) {
-                _read(&value, sizeof(T), T::packet_id);
-            }
-            else {
-                _read(&value, sizeof(T));
-            }
+            _read(&value, sizeof(T));
         }
 
         template<class T>
@@ -48,27 +28,12 @@ namespace smon {
             _write(&value, sizeof(T));
         }
 
-        bool has_data();
-
-        void reset();
-
     private:
-
-        std::mutex mutex;
 
         void* serial;
         void* io;
 
-        static constexpr auto buffer_size = 2048;
-        unsigned unread_count = 0;
-        unsigned read_index = 0;
-        unsigned write_index = 0;
-
-        std::array<uint8_t, buffer_size> data_buffer;
-
-        std::list<DataBuffer> received_packets;
-
-        void _read(void* buffer, unsigned size, uint16_t id = -1);
+        void _read(void* buffer, unsigned size);
 
         void _write(const void* buffer, unsigned size);
 
---
+++ source/smon/SerialTransmitter.cpp
@@ -0,0 +1,139 @@
+
+#include <boost/asio.hpp>
+
+#include "PacketHeader.hpp"
+#include "SerialTransmitter.hpp"
+#include "ExceptionCatch.hpp"
+
+#define SMON_DONT_THROW_ON_CONNECTION_ERRORS
+
+using namespace cu;
+
+using namespace std;
+using namespace boost;
+using namespace boost::asio;
+
+using namespace smon;
+
+#define __SERIAL static_cast<serial_port*>(serial)
+#define __IO static_cast<io_service*>(io)
+
+static bool stop = false;
+
+SerialMonitorOld::SerialMonitorOld(const string& port, unsigned baud_rate) {
+    stop = false;
+    io = new io_service();
+
+    try {
+        serial = new serial_port(*__IO, port);
+        __SERIAL->set_option(serial_port_base::baud_rate(baud_rate));
+    }
+    catch(...) {
+        Log(what());
+#ifdef SMON_DONT_THROW_ON_CONNECTION_ERRORS
+        failed_init = true;
+        return;
+#else
+        Fatal(what());
+#endif
+    }
+
+    std::thread([&] {
+
+        while(true) {
+
+            if (stop) return;
+
+            static PacketHeader header;
+
+            static uint8_t byte;
+            asio::read(*__SERIAL, buffer(&byte, 1));
+            header.add_byte(byte);
+
+            if (header.is_valid()) {
+                DataBuffer packet;
+                packet.size = header.size;
+                packet.packet_id = header.packet_id;
+                // Logvar(packet.size);
+                for (unsigned i = 0; i < header.size; i++) {
+                    asio::read(*__SERIAL, buffer(&byte, 1));
+                    packet.data[i]  = byte;
+                }
+
+                this->mutex.lock();
+                received_packets.push_back(packet);
+                this->mutex.unlock();
+            }
+
+        }
+
+    }).detach();
+}
+
+SerialMonitorOld::~SerialMonitorOld() {
+#ifdef SMON_DONT_THROW_ON_CONNECTION_ERRORS
+    if (failed_init) return;
+#endif
+    stop = true;
+    mutex.lock();
+    delete __SERIAL;
+    delete __IO;
+    mutex.unlock();
+}
+
+bool SerialMonitorOld::has_data() {
+    return !received_packets.empty();
+}
+
+void SerialMonitorOld::_read(void* buf, unsigned size, uint16_t id) {
+
+#ifdef SMON_DONT_THROW_ON_CONNECTION_ERRORS
+    if (failed_init) return;
+#endif
+
+    if (id == -1) {
+        Fatal("Not implemented yet");
+    }
+
+    mutex.lock();
+
+    if (received_packets.size() == 0) {
+        mutex.unlock();
+        Log("No packets");
+        return;
+    }
+
+    DataBuffer* packet_ptr = nullptr;
+
+    auto matched_packet = std::find_if(received_packets.begin(), received_packets.end(), [&](const DataBuffer& packet) {
+        return packet.packet_id == id;
+    });
+
+    if (matched_packet == received_packets.end()) {
+        mutex.unlock();
+        Log("Invalid packet");
+        return;
+    }
+
+    packet_ptr = &(*matched_packet);
+
+    DataBuffer& packet = *matched_packet;
+
+    memcpy(buf, &packet.data[0], size);
+
+    received_packets.erase(matched_packet);
+
+    mutex.unlock();
+
+    return;
+}
+
+void SerialMonitorOld::_write(const void* buf, unsigned size) {
+#ifdef SMON_DONT_THROW_ON_CONNECTION_ERRORS
+    if (failed_init) return;
+#endif
+    mutex.lock();
+    asio::write(*__SERIAL, buffer(buf, size));
+    bytes_sent += size;
+    mutex.unlock();
+}
